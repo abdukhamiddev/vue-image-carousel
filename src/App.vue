dbebee43e85968dcd6729515d8bbb2242b18629d
abdukhamid <latipovabdukhamid@gmail.com>
2022-08-29 21:34:12 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+// import items from "@/data/items.json";
+import items from "./data/images.json";
+import Slider from "./components/index.vue";
+</script>
+  
+  <template>
+  <Slider :items="items" />
+</template>
